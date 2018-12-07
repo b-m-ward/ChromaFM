c093772824e1c76de4155fa93111e0061800dbc9
Yourlockhead <bryantmward@gmail.com>
2018-12-06 23:35:02 -0500

    enhance: add rest of soma stations

--- background_player.js
+++ background_player.js
@@ -4,9 +4,6 @@ chrome.extension.onMessage.addListener(
             document.write('<audio id="player">');
             document.getElementById('player').src = request.channel;
             document.getElementById('player').play();
-            source1.src= request.channel;
-            audioElement.load;
-            audioElement.play();
         }
 
         if(request.action == "pause") {
--- player.js
+++ player.js
@@ -1,15 +1,26 @@
 console.log('loaded player.js');
 
+var station = "http://ice1.somafm.com/groovesalad-256-mp3"
+
 var launchButton = document.getElementById('launch-button');
 
 launchButton.addEventListener('click', () => {
     console.log('play button clicked');
-    chrome.extension.sendMessage({action: "play", channel: "http://ice1.somafm.com/groovesalad-256-mp3"});
+    chrome.extension.sendMessage({action: "play", channel: station});
+    chrome.browserAction.setBadgeText({text: ">"});
 });
 
-var pauseButton = document.getElementById('pause-button')
+var pauseButton = document.getElementById('pause-button');
 
 pauseButton.addEventListener('click', () => {
     console.log('pause button clicked');
     chrome.extension.sendMessage({action: "pause"});
+    chrome.browserAction.setBadgeText({text: "="});
+});
+
+var selectStation = document.getElementById('select-station');
+
+selectStation.addEventListener('change', (e) => {
+    console.log(e.srcElement.value);
+    station = e.srcElement.value;
 });
